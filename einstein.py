3d897032b11aa84f91b46edd3f6b109ed8932e7e
Gibs98 <83430393+Gibs98@users.noreply.github.com>
2025-02-01 22:10:18 -0600

    Added homework
    .

---
+++ einstein.py
@@ -0,0 +1,7 @@
+#implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number of Joules as an integer
+m=input("input an integer here ")
+m=int(m)
+#e=mc2
+c=300000000
+e=m*c*c
+print(e)
