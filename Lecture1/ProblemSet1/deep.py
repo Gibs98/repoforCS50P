f9c95522afa42d9839b65fd87e3b8ff699fa951a
Gibs98 <83430393+Gibs98@users.noreply.github.com>
2025-02-08 21:46:45 -0600

    Update deep.py

--- Lecture1/ProblemSet1/deep.py
+++ Lecture1/ProblemSet1/deep.py
@@ -4,19 +4,15 @@
 #Summary: A program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
 
 
-
 #Define answer 
 def answer(n):
-    if n == "42" or "forty-two" or "forty two":
+    if n == ("42" or "forty-two" or "forty two"):
         print("yes")
     else:
         print("no")
 #a main function   
 def main():
     question = "What is the great question of life and the meaning of everything?"
-
-text = input(question)
-
-
-answer(text.lower())
+    text = input(question)
+    answer(text.lower())
 main()
